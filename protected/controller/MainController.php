51647b9f5b2680bfb66db8b197a2d881128d32c6
Ervin Hua <yhua.lin@qq.com>
2017-11-08 23:20:50 +0800

    留言列表模块

--- protected/controller/MainController.php
+++ protected/controller/MainController.php
@@ -1,7 +1,19 @@
 <?php
 class MainController extends BaseController {
     function actionIndex(){
+        // 实例化一个guestbook的模型类
+        $guestbook = new Model("guestbook");
+        // 用findAll()方法查询guestbook表的全部数据
+        $this->records = $guestbook->findAll();
+        // 输出看看
+        //dump($this->records);
+        // 为了清楚到输出，我们的先屏蔽页面输出
         $this->display("guestbook.html");
+        /**
+         * $this->records这样的变量是控制器的成员变量，而这个写法跟普遍变量（比如说$records）不一样的地方是，$this->records可以在模板内直接使用。
+         *  也就是说，在控制器内，通过$this->赋值的变量，都可以在模板内使用；而普通的变量并没有这样的功能。
+         *  这也是最基础的模板引擎的用法之一。
+         */
     }
     function actionWrite(){
         // 这种dump()提交数据的方式建议大家多用用，这是非常有益的小习惯。
