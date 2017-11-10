d99bd892b42495dfb185494c465350ec1c2e7c13
yhua <yhua.lin@qq.com>
2017-11-10 15:34:42 +0800

    修改管理首页统计

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,6 +5,18 @@ use Admin\Controller;
 class IndexController extends BaseController{
 
     public function index(){
+    	$User 		= M("member");		// 实例化Manber对象
+    	$userCount 	= $User->count();	// 获取用户数
+    	$Post 		= M("post");		// 实例化Page对象
+    	$postCount	= $Post->count();	// 获取文章数
+    	$Links		= M("links");		// 实例化Links对象
+    	$linksCount = $Links->count();	// 获取链接数
+
+    	$data['usercount']	= $userCount;
+    	$data['postcount']	= $postCount;
+    	$data['linkscount'] = $linksCount;
+
+    	$this->assign('data',$data);
         $this->display();
     }
 }
