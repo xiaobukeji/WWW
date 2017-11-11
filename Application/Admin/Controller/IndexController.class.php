7f1a5b89a6df5959ba7b1f64aca1f92850109928
yhua <yhua.lin@qq.com>
2017-11-11 11:25:23 +0800

    管理首页统计增加留言统计

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,16 +5,19 @@ use Admin\Controller;
 class IndexController extends BaseController{
 
     public function index(){
-    	$User 		= M("member");		// 实例化Manber对象
-    	$userCount 	= $User->count();	// 获取用户数
-    	$Post 		= M("post");		// 实例化Page对象
-    	$postCount	= $Post->count();	// 获取文章数
-    	$Links		= M("links");		// 实例化Links对象
-    	$linksCount = $Links->count();	// 获取链接数
+    	$User 		        = M("member");		    // 实例化Manber对象
+    	$userCount 	        = $User->count();	    // 获取用户数
+    	$Post 		        = M("post");		    // 实例化Page对象
+    	$postCount	        = $Post->count();	    // 获取文章数
+    	$Links		        = M("links");		    // 实例化Links对象
+    	$linksCount         = $Links->count();	    // 获取链接数
+        $GuestBook          = M("guestbook");       // 实例化guestbook对象
+        $guestBookCount     = $GuestBook->count();  // 获取留言数
 
-    	$data['usercount']	= $userCount;
-    	$data['postcount']	= $postCount;
-    	$data['linkscount'] = $linksCount;
+    	$data['usercount']	    = $userCount;
+    	$data['postcount']	    = $postCount;
+    	$data['linkscount']     = $linksCount;
+        $data['guestbookcount'] = $guestBookCount;
 
     	$this->assign('data',$data);
         $this->display();
